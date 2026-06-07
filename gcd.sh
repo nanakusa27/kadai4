76a5a1b074debc2effb21da38042a670a4bddf72
m-wata <a26141mw@aiit.ac.jp>
2026-06-07 19:44:51 +0900

    UPDATE:gcd.sh
    異常系の処理を追加

--- gcd.sh
+++ gcd.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+	echo "ERR: 引数が2つではない" >&2
+	exit 1
+fi
+
+expr "$1" + "$2" > /dev/null 2>&1
+if [ $? -ge 2 ]; then
+	echo "ERR: 引数が数値ではない" >&2
+	exit 1
+fi
+
+if [ "$1" -le 0 ] || [ "$2" -le 0 ]; then
+       echo "ERR: 引数の数値が1以上ではない" >&2
+       exit 1
+fi
+
+
 a=$1
 b=$2
 
